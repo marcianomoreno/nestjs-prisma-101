480c454c3c752bf6df7b1a45b0786729d8f409d7
Marciano Moreno <marciano_moreno@outlook.com>
2022-04-21 14:12:01 -0500

    Implemented user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user.controller';
 import { PostController } from './post.controller';
+import { UserService } from './user.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UserController, PostController],
-  providers: [AppService],
+  providers: [AppService, UserService, PrismaService],
 })
 export class AppModule {}
--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User as UserModel } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  getUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @Get()
+  getUsers(): Promise<UserModel[]> {
+    return this.userService.users({});
+  }
+
+  @Post()
+  createUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Param('id') id: number,
+    @Body() userData: { name?: string; email: string },
+  ) {
+    return this.userService.updateUser({
+      where: { id: id },
+      data: userData,
+    });
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    console.log(id);
+    return this.userService.deleteUser({
+      where: { id: Number(id) },
+    });
+  }
+}
